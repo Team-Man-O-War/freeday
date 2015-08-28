cd547b80ae4c331018499628a3780ac178586e6d
MIKE SHOBE <mikeshobe@yahoo.com>
2015-08-27 17:40:07 -0700

    removed api from html

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 //path? fs? bodyparser? will need FB login & db schema.
 
-app.use('/', express.static('client'));//should serve index.html page.
+app.use('/', express.static('client'));
 
 app.listen(3000);
