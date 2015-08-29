9f6ff9160c325bf3e5b3e2f95a9cd0f837fa3da7
Arash Dai <arashdai@gmail.com>
2015-08-28 17:01:33 -0700

    database is set up and synced

--- server/server.js
+++ server/server.js
@@ -1,50 +1,51 @@
 var express = require('express');
 var app = express();
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('postgres://localhost/freedaydb');
+var sequelize = new Sequelize('postgres://postgres:password@localhost:5432/freedayDB');
 
 
 
 var User = sequelize.define('user',{
 
 	username:{
-		type: sequelize.STRING,
+		type: Sequelize.STRING,
 		field:'username'
 	},
 	password:{
-		type: sequelize.STRING,
+		type: Sequelize.STRING,
 		field:'password'
 	},
 	address:{
-		type:sequelize.STRING,
+		type:Sequelize.STRING,
 		field:'address'
-	}
-})
+	},
+
+});
 
 var Event = sequelize.define('event',{
 
   title:{
-    type:sequelize.STRING,
+    type:Sequelize.STRING,
     field:'title'
   },
   category:{
-    type:sequelize.STRING,
+    type:Sequelize.STRING,
     field:'category'
   },
   time:{
-    type:sequelize.STRING,
+    type:Sequelize.STRING,
     field:'time'
   },
   description:{
-    type:sequelize.STRING,
+    type:Sequelize.STRING,
     field:'description'
   },
   location:{
-    type:sequelize.STRING,
+    type:Sequelize.STRING,
     field:'location'
   },
   tags:{
-    type: DataTypes.ARRAY(DataTypes.STRING),
+    type: Sequelize.ARRAY(Sequelize.STRING),
     field:'tags'
   }
 
@@ -52,9 +53,37 @@ var Event = sequelize.define('event',{
 });
 
 var Category = sequelize.define('category',{
-  name:sequelize.STRING,
+  name:Sequelize.STRING,
   field:'name'
 });
+
+//here we have to set up user.sync, we sync our schemas to become tables
+User.sync({force:true}).then(function(){
+  //table is created
+  return User.create({
+    username: 'arash',
+    password: 'arash is cool'
+
+  })
+});
+
+Event.sync({force:true}).then(function(){
+  //table is created
+  // return Event.create({
+  //   title:
+  //   category:
+  //   time:
+  //   description:
+  //   location:
+  //   tags:
+  // })
+
+});
+
+
+//here we set up the many to many relationship, and name the table UserEvent.
+User.belongsToMany(Event,{as:'party',through:'attendee_party',foreignKey:'userId'});
+Event.belongsToMany(User,{as:'attendee',through:'attendee_party',foreignKey:'eventId'});
 
 
 
