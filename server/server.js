862d79cafe69a63956feeb6cd1241aeb7f81de73
RedWood <mackey91@gmail.com>
2015-09-01 18:43:28 -0700

    geolocation works

--- server/server.js
+++ server/server.js
@@ -8,10 +8,26 @@ var config = require('./config/config.js');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/map', function(req, res) {
-request.get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=' + config.googleMapApi.key, function(err, response, body) {
-    res.send(body);
-  });
+// request({
+//     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=' + config.googleMapApi.key, //URL to hit
+//     //qs: {from: 'blog example', time: +new Date()}, //Query string data
+//     method: 'GET', //Specify the method
+//     headers: { //We can define headers too
+//         'Content-Type': 'application/json'
+//     }
+// }, function(error, response, body){
+//     if(error) {
+//         console.log(error);
+//     } else {
+//         console.log(response.statusCode, body);
+//     }
+// });
+
+var map = 'https://www.googleapis.com/geolocation/v1/geolocate?key=' + config.googleMapApi.key;
+app.post('/map', function(req, res) {
+  request.get({map: map}, function(err, response, body) {
+      res.send(map);
+    });
 });
 
 // if (sequelize.sync()) {
@@ -24,8 +40,6 @@ app.use(express.static('client'));//should serve index.html page.
 // var connect = require('./db/connection.js');
 // var router = require('./router.js');
 // requires below I have added for jsonwebtokens -arash
-
-app.use(express.static('client'));
 
 // app.use(router);
 
